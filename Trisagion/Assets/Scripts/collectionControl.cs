7e39d92a9349b7363a98724707cd419e3c61c902
dubaodan <47341203+dubaodan@users.noreply.github.com>
2020-04-07 17:38:40 +0800

    人物动画制作

---
+++ Trisagion/Assets/Scripts/collectionControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectionControl : MonoBehaviour
+{
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+    }
+
+    void SwitchAnim()
+    {
+        anim.SetBool("destory", true);
+    }
+
+    void DestoryThis()
+    {
+        Destroy(gameObject);
+    }
+}
--- Trisagion/Assets/Scripts/playerControl.cs
+++ Trisagion/Assets/Scripts/playerControl.cs
@@ -1,26 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playerControl : MonoBehaviour
 {
-    public Rigidbody2D Rb2D;
-    public Collider2D coll;
-    public Animator anim;
+    private Rigidbody2D Rb2D;
+    private Animator anim;
+
     public float speed;
     public float jumpForce;
     public Transform groundCheck;
     public LayerMask ground;
+    public Text scoresText;
+    public Text hpText;
 
+    [SerializeField]private int  score = 0;
 
-    public bool isGround;
+    int playerHp = 3;
+    bool isGround,isFall;
     bool jumpPressed;
-    public int jumpCount = 2;
+    int jumpCount = 2;
+    bool isHurt = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rb2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        scoresText.text = "Score:0" ;
+        hpText.text = "Hp:"+playerHp.ToString();
     }
 
     //使动作更加平滑
@@ -28,8 +37,12 @@ public class playerControl : MonoBehaviour
     {
         isGround = Physics2D.OverlapCircle(groundCheck.position, 0.1f, ground);
 
-        playerMove();
-        playerJump();
+        if (!isHurt)
+        {
+            PlayerMove();
+            PlayerJump();
+        }
+        SwitchAnim();
     }
 
 
@@ -43,11 +56,10 @@ public class playerControl : MonoBehaviour
     }
 
     //角色移动
-    void playerMove()
+    void PlayerMove()
     {
         float horizontalMove = Input.GetAxisRaw("Horizontal");
         Rb2D.velocity = new Vector2(horizontalMove * speed, Rb2D.velocity.y);
-        anim.SetFloat("running", Mathf.Abs(horizontalMove));
         if (horizontalMove != 0)
         {
             transform.localScale = new Vector3(horizontalMove, 1, 1);
@@ -55,12 +67,13 @@ public class playerControl : MonoBehaviour
     }
 
     //角色跳跃
-    void playerJump()
+    void PlayerJump()
     {
         if(isGround)
         {
             jumpCount = 2;
             isGround =true;
+            isFall = true;
         }
         if(jumpPressed && isGround)
         {
@@ -68,12 +81,111 @@ public class playerControl : MonoBehaviour
             Rb2D.velocity = new Vector2(Rb2D.velocity.x, jumpForce);
             jumpCount--;
             jumpPressed = false;
+            isFall = false;
         }
         else if(jumpPressed && jumpCount>0 && !isGround)
         {
             Rb2D.velocity = new Vector2(Rb2D.velocity.x, jumpForce);
             jumpCount--;
             jumpPressed = false;
+            isFall = false;
+        }
+        else if(!jumpPressed && !isGround && isFall)
+        {
+            jumpCount--;
+            isFall = false;
+        }
+    }
+
+    //切换动画状态
+    void SwitchAnim()
+    {
+        if (isHurt)
+        {
+            anim.SetBool("hurting", true);
+            return;
+        }
+
+        anim.SetFloat("running", Mathf.Abs(Rb2D.velocity.x));
+
+        if (isGround)
+        {
+            anim.SetBool("falling", false);
+        }
+        else if(!isGround && Rb2D.velocity.y > 0)
+        {
+            anim.SetBool("jumpping", true);
+        }
+        else if(Rb2D.velocity.y < 0)
+        {
+            anim.SetBool("jumpping", false);
+            anim.SetBool("falling", true);
+        }
+    }
+
+    //动画：从受伤状态恢复
+    void RecFromDamage()
+    {
+        anim.SetInteger("playerHp", playerHp);
+        if (playerHp > 0) 
+        {
+            anim.SetBool("hurting", false);
+            isHurt = false;
+        }
+    }
+    //动画：消失后销毁
+    void DestoryThis()
+    {
+        Destroy(gameObject);
+    }
+
+    //触发器，吃到食物的时候
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isHurt) { return; }
+        switch (collision.tag)
+        {
+            case "Collections2":
+                collision.GetComponent<BoxCollider2D>().enabled = false;
+                collision.SendMessage("SwitchAnim");
+                score += 500;
+                scoresText.text = "Score:" + score.ToString();
+                break;
+            case "Collections1":
+                collision.GetComponent<BoxCollider2D>().enabled = false;
+                collision.SendMessage("SwitchAnim");
+                score += 200;
+                scoresText.text = "Score:" + score.ToString();
+                break;
+            case "Collections":
+                collision.GetComponent<BoxCollider2D>().enabled = false;
+                collision.SendMessage("SwitchAnim");
+                score += 100;
+                scoresText.text = "Score:" + score.ToString();
+                break;
+            case "Traps":
+                isHurt = true;
+                playerHp--;
+                hpText.text = "Hp:" + playerHp.ToString();
+                if (transform.position.x < collision.gameObject.transform.position.x)
+                {
+                    Rb2D.velocity = new Vector2(-5, Rb2D.velocity.y);
+                }
+                else if (transform.position.x > collision.gameObject.transform.position.x)
+                {
+                    Rb2D.velocity = new Vector2(5, Rb2D.velocity.y);
+                }
+                if (transform.position.y < collision.gameObject.transform.position.y)
+                {
+                    Rb2D.velocity = new Vector2(Rb2D.velocity.x, -5);
+                }
+                else if (transform.position.y > collision.gameObject.transform.position.y)
+                {
+                    Rb2D.velocity = new Vector2(Rb2D.velocity.x, 5);
+                }
+                break;
+            default:
+                break;
         }
     }
 
